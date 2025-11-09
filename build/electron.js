4598d80d4f9446435b4c78b70629133def084521
duongpham17 <duongpham17@icloud.com>
2025-11-09 21:04:10 +0000

    first commit

--- build/electron.js
+++ build/electron.js
@@ -1,10 +1,14 @@
-const development = require('./electron-development');
+// const development = require('./electron-development');
+// const production = require('./electron-production');
+
+// const isProduction = process.env.NODE_ENV === "production";
+
+// if(isProduction){
+//     production.start();
+// } else {
+//     development.start();
+// }
+
 const production = require('./electron-production');
 
-const isProduction = process.env.NODE_ENV === "production";
-
-if(isProduction){
-    production.start();
-} else {
-    development.start();
-}
+production.start();
