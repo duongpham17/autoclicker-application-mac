fbcaafa6bcdebf4a92a92f68d6debe32c81d3879
duongpham17 <duongpham17@icloud.com>
2025-11-10 13:59:32 +0000

    testing

--- src/pages/dashkboard/scripts/main/index.tsx
+++ src/pages/dashkboard/scripts/main/index.tsx
@@ -1,17 +1,20 @@
 import { Fragment, useContext } from 'react';
 import { Context } from '../../Context';
 import Text from '@components/texts/Style1';
+import Flex from '@components/flex/Style1';
+import { BsArrowLeft } from "react-icons/bs";
 
 import Informations from './informations';
 import Commands from './commands';
 import Actions from './actions';
 import Edit from './edit';
 
+
 const Main = () => {
 
   const {script} = useContext(Context);
 
-  if(!script) return <div><Text message="<- Select a script to start."/></div>
+  if(!script) return <Flex><BsArrowLeft/><Text message="Select a script to start."/></Flex>
 
   return (
   <Fragment>
